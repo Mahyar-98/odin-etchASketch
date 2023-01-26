6f9e9f5b3c5db31e1c3be32b89f44d6622075a19
Mahyar <mahyar.erfanian1998@gmail.com>
2023-01-26 17:45:33 -0500

    Add description to README file and add the reset button at the top of the homepage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-etchASketch
 The Odin Project - Foundations Course - Project: Etch-A-Sketch
+
+
+This project aims to examine my knowledge of basic DOM manipulation using JavaScript.
--- script.js
+++ script.js
@@ -1,17 +1,37 @@
+const resetBtn = document.querySelector(".reset-btn");
 const body = document.querySelector("body");
-const mainDiv = document.createElement("div");
-body.appendChild(mainDiv)
-for (let i = 0; i < 16; i++) {
-  mainDiv.appendChild(document.createElement("div"));
-  for (let j = 0; j < 16; j++) {
-    mainDiv.lastChild.appendChild(document.createElement("div"));
+const newGrid = document.createElement("div");
+let divs = null;
+body.appendChild(newGrid);
+makeGrid(16);
+
+resetBtn.addEventListener("click", e => {
+  let askDimension = prompt("Please enter the dimension of your grid (max 100).")
+  if (askDimension > 0 && askDimension <= 100) {
+    makeGrid(askDimension)
   }
+})
+
+
+function makeGrid(size) {
+  oldGrid = document.querySelector("body > div");
+  oldGrid.innerHTML = null;
+  body.removeChild(oldGrid);
+  body.appendChild(newGrid);
+  for (let i = 0; i < size; i++) {
+    newGrid.appendChild(document.createElement("div"));
+    for (let j = 0; j < size; j++) {
+      newGrid.lastChild.appendChild(document.createElement("div"));
+    }
+  }
+  divs = newGrid.querySelectorAll("body > div > div > div");
+  eventListen()
 }
 
-const divs = mainDiv.querySelectorAll("body > div > div > div");
-console.log(divs)
 
-divs.forEach(div => {
-  div.addEventListener("mouseover", e => e.target.classList.toggle("div-active")); 
-  div.addEventListener("mouseout", e => e.target.classList.toggle("div-active"));  
-});
+function eventListen() {
+  divs.forEach(div => {
+    div.addEventListener("mouseover", e => e.target.classList.toggle("div-active")); 
+    div.addEventListener("mouseout", e => e.target.classList.toggle("div-active"));  
+  });
+}
